3ca0d1a0770192911cf53b5c20192210fd8dbaf3
beepscore <support@beepscore.com>
2014-11-27 23:11:29 -0800

    In PhotoGalleryActivity extends change Activity to SingleFragmentActivity. Add SingleFragmentActivity, PhotoGalleryFragment, activity_fragment, fragment_photo_gallery.
    Delete activity_photo_gallery.

---
+++ app/src/main/java/com/beepscore/android/photogallery/PhotoGalleryFragment.java
@@ -0,0 +1,31 @@
+package com.beepscore.android.photogallery;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.GridView;
+
+/**
+ * Created by stevebaker on 11/26/14.
+ */
+public class PhotoGalleryFragment extends Fragment {
+    GridView mGridView;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRetainInstance(true);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_photo_gallery, container, false);
+        mGridView = (GridView)view.findViewById(R.id.gridView);
+
+        return view;
+    }
+
+}
