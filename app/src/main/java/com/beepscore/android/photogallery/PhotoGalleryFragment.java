18c2e52e498294f2e1e4091375ef1cdd77f21058
beepscore <support@beepscore.com>
2014-12-01 11:50:27 -0800

    In PhotoGalleryFragment add setupAdapter. In FetchItemsTask revise parameters, override onPostExecute. Now app parses xml and displays it.

--- app/src/main/java/com/beepscore/android/photogallery/PhotoGalleryFragment.java
+++ app/src/main/java/com/beepscore/android/photogallery/PhotoGalleryFragment.java
@@ -7,9 +7,11 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.GridView;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 /**
  * Created by stevebaker on 11/26/14.
@@ -17,6 +19,7 @@ import java.io.IOException;
 public class PhotoGalleryFragment extends Fragment {
     private static final String TAG = "PhotoGalleryFragment";
     GridView mGridView;
+    ArrayList<GalleryItem> mItems;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -32,16 +35,54 @@ public class PhotoGalleryFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_photo_gallery, container, false);
         mGridView = (GridView)view.findViewById(R.id.gridView);
+        setupAdapter();
 
         return view;
     }
 
-    private class FetchItemsTask extends AsyncTask<Void,Void,Void> {
+    /**
+     * Call in onCreateView to handle rotation
+     * Call when model changes to update view
+     */
+    void setupAdapter() {
+        // check if fragment is attached to an Activity
+        // when using AsyncTask fragment may not be attached to an Activity
+        if (getActivity() == null ||
+                mGridView == null) {
+            return;
+        }
 
+        if (mItems != null) {
+            mGridView.setAdapter(new ArrayAdapter<GalleryItem>(getActivity(),
+                    android.R.layout.simple_gallery_item, mItems));
+        } else {
+            mGridView.setAdapter(null);
+        }
+    }
+
+    /**
+     * third parameter is AsyncTask result type
+     * matches doInBackground return type and onPostExecute input type
+     */
+    private class FetchItemsTask
+            extends AsyncTask<Void,Void,ArrayList<GalleryItem>> {
+
+        /**
+         * executes on background thread, so can't update UI safely
+         * Android won't let app update UI from a background thread
+         */
         @Override
-        protected Void doInBackground(Void... params) {
-            new FlickrFetchr().fetchItems();
-            return null;
+        protected ArrayList<GalleryItem> doInBackground(Void... params) {
+            return new FlickrFetchr().fetchItems();
+        }
+
+        /**
+         * callback on main thread (UI thread), so can update UI safely
+         */
+        @Override
+        protected void onPostExecute(ArrayList<GalleryItem> items) {
+            mItems = items;
+            setupAdapter();
         }
     }
 
