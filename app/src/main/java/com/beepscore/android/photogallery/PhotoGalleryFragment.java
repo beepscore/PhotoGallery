2bb8e4fc9b1474f04f6ce0958415df44d6fd7f4f
beepscore <support@beepscore.com>
2014-12-04 17:01:55 -0800

    In PhotoGalleryFragment add inner class GalleryItemAdapter and in setupAdapter instantiate it. Now app shows placeholder images instead of list of text.

--- app/src/main/java/com/beepscore/android/photogallery/PhotoGalleryFragment.java
+++ app/src/main/java/com/beepscore/android/photogallery/PhotoGalleryFragment.java
@@ -3,14 +3,13 @@ package com.beepscore.android.photogallery;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.GridView;
+import android.widget.ImageView;
 
-import java.io.IOException;
 import java.util.ArrayList;
 
 /**
@@ -53,10 +52,30 @@ public class PhotoGalleryFragment extends Fragment {
         }
 
         if (mItems != null) {
-            mGridView.setAdapter(new ArrayAdapter<GalleryItem>(getActivity(),
-                    android.R.layout.simple_gallery_item, mItems));
+            mGridView.setAdapter(new GalleryItemAdapter(mItems));
         } else {
             mGridView.setAdapter(null);
+        }
+    }
+
+    private class GalleryItemAdapter extends ArrayAdapter<GalleryItem> {
+
+        public GalleryItemAdapter(ArrayList<GalleryItem> items) {
+            super(getActivity(), 0, items);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            if (convertView == null) {
+                convertView = getActivity().getLayoutInflater()
+                        .inflate(R.layout.gallery_item, parent, false);
+            }
+
+            ImageView imageView = (ImageView)convertView
+                    .findViewById(R.id.gallery_item_imageView);
+            imageView.setImageResource(R.drawable.brian_up_close);
+
+            return imageView;
         }
     }
 
